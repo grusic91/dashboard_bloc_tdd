e430d79efc9ef37bf82c5f4431da921e41481336
Grus <gasper.rus@programmfabrik.de>
2024-01-30 09:49:45 +0100

    Adds placeholder page, media resources class, router.

---
+++ lib/core/services/router.dart
@@ -0,0 +1,31 @@
+import 'package:dashboard_bloc_tdd/core/common/views/page_under_construction.dart';
+import 'package:dashboard_bloc_tdd/src/on_boarding/presentation/on_boarding_screen.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case OnBoardingScreen.routeNmae:
+      return _pageBuilder(
+        (_) => const OnBoardingScreen(),
+        settings: settings,
+      );
+
+    default:
+      return _pageBuilder(
+        (_) => const PageUnderConstruction(),
+        settings: settings,
+      );
+  }
+}
+
+PageRouteBuilder<dynamic> _pageBuilder(Widget Function(BuildContext) page,
+    {required RouteSettings settings}) {
+  return PageRouteBuilder(
+    settings: settings,
+    transitionsBuilder: (_, animation, __, child) => FadeTransition(
+      opacity: animation,
+      child: child,
+    ),
+    pageBuilder: (context, _, __) => page(context),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:dashboard_bloc_tdd/core/res/colors.dart';
 import 'package:dashboard_bloc_tdd/core/res/fonts.dart';
+import 'package:dashboard_bloc_tdd/core/services/router.dart';
+import 'package:dashboard_bloc_tdd/src/on_boarding/presentation/on_boarding_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,6 +23,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      onGenerateRoute: generateRoute,
     );
   }
 }
