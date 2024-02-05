287f8d4191204b86c751bd3ba68afbfa327bf53d
Grus <gasper.rus@programmfabrik.de>
2024-02-05 23:25:24 +0100

    Adds implementation of sign_up_test

---
+++ test/src/auth/domain/usecases/sign_up_test.dart
@@ -0,0 +1,52 @@
+import 'package:dartz/dartz.dart';
+import 'package:dashboard_bloc_tdd/src/auth/domain/usecases/sign_up.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'auth_repo.mock.dart';
+
+void main() {
+  late MockAuthRepo repo;
+  late SignUp usecase;
+
+  const tEmail = 'Test email';
+  const tPassword = 'Test password';
+  const tFullName = 'Test full name';
+
+  setUp(() {
+    repo = MockAuthRepo();
+    usecase = SignUp(repo);
+  });
+
+  test('should call the [AuthRepo.signUp]', () async {
+    when(
+      () => repo.signUp(
+        email: any(named: 'email'),
+        fullName: any(named: 'fullName'),
+        password: any(named: 'password'),
+      ),
+    ).thenAnswer(
+      (_) async => const Right(null),
+    );
+
+    final result = await usecase(
+      const SignUpParams(
+        email: tEmail,
+        fullName: tFullName,
+        password: tPassword,
+      ),
+    );
+
+    expect(result, const Right<dynamic, void>(null));
+
+    verify(
+      () => repo.signUp(
+        email: tEmail,
+        fullName: tFullName,
+        password: tPassword,
+      ),
+    );
+
+    verifyNoMoreInteractions(repo);
+  });
+}
