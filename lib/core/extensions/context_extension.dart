826fb7a7cfc2469c1ef1be59c3a3447f23756eba
Grus <gasper.rus@programmfabrik.de>
2024-02-05 17:23:14 +0100

    Adds mediaquery getters.

--- lib/core/extensions/context_extension.dart
+++ lib/core/extensions/context_extension.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
 extension ContextExt on BuildContext {
+  // Theme
   ThemeData get theme => Theme.of(this);
+
+  // MediaQuery
+  MediaQueryData get mediaQuery => MediaQuery.of(this);
+
+  // get size
+  Size get size => mediaQuery.size;
+
+  // get widths
+  double get width => size.width;
+
+  // get height
+  double get height => size.height;
 }
