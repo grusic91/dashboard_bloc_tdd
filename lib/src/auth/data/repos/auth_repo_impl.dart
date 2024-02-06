22ddc8154a49282071adbad958e3197f4ca715d3
Grus <gasper.rus@programmfabrik.de>
2024-02-06 21:53:35 +0100

    Adds AuthRepoImplementation that implements AuthRepo.

---
+++ lib/src/auth/data/repos/auth_repo_impl.dart
@@ -0,0 +1,91 @@
+import 'package:dartz/dartz.dart';
+import 'package:dashboard_bloc_tdd/core/enums/update_user.dart';
+import 'package:dashboard_bloc_tdd/core/errors/exceptions.dart';
+import 'package:dashboard_bloc_tdd/core/errors/failures.dart';
+import 'package:dashboard_bloc_tdd/core/utils/typdefs.dart';
+import 'package:dashboard_bloc_tdd/src/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:dashboard_bloc_tdd/src/auth/domain/entities/user.dart';
+import 'package:dashboard_bloc_tdd/src/auth/domain/repos/auth_repo.dart';
+
+class AuthRepoImpl implements AuthRepo {
+  const AuthRepoImpl(this._remoteDataSource);
+
+  final AuthRemoteDataSource _remoteDataSource;
+
+  @override
+  ResultFuture<void> forgotPassword(String email) async {
+    try {
+      await _remoteDataSource.forgotPassword(email);
+      return const Right(null);
+    } on ServerException catch (e) {
+      return Left(
+        ServerFailure(message: e.message, statusCode: e.statusCode),
+      );
+    }
+  }
+
+  @override
+  ResultFuture<LocalUser> signIn({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final result = await _remoteDataSource.signIn(
+        email: email,
+        password: password,
+      );
+      return Right(result);
+    } on ServerException catch (e) {
+      return Left(
+        ServerFailure(
+          message: e.message,
+          statusCode: e.statusCode,
+        ),
+      );
+    }
+  }
+
+  @override
+  ResultFuture<void> signUp({
+    required String email,
+    required String fullName,
+    required String password,
+  }) async {
+    try {
+      await _remoteDataSource.signUp(
+        email: email,
+        fullName: fullName,
+        password: password,
+      );
+      return const Right(null);
+    } on ServerException catch (e) {
+      return Left(
+        ServerFailure(
+          message: e.message,
+          statusCode: e.statusCode,
+        ),
+      );
+    }
+  }
+
+  @override
+  ResultFuture<void> updateUser({
+    required UpdateUserAction action,
+    dynamic userData,
+  }) async {
+    try {
+      await _remoteDataSource.updateUser(
+        action: action,
+        userData: userData,
+      );
+      return const Right(null);
+    } on ServerException catch (e) {
+      return Left(
+        ServerFailure(
+          message: e.message,
+          statusCode: e.statusCode,
+        ),
+      );
+    }
+  }
+}
