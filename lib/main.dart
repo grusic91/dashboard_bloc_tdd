7db2e03843e2d9a8c640eeb48a63addd468d6be8
Grus <gasper.rus@programmfabrik.de>
2024-02-08 15:14:32 +0100

    adds and configure packages for using firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,18 @@ import 'package:dashboard_bloc_tdd/core/res/colors.dart';
 import 'package:dashboard_bloc_tdd/core/res/fonts.dart';
 import 'package:dashboard_bloc_tdd/core/services/injection_container.dart';
 import 'package:dashboard_bloc_tdd/core/services/router.dart';
+import 'package:dashboard_bloc_tdd/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+// ...
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await init();
   runApp(const MyApp());
 }
