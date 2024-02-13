30a6a45eac0178b0c0cbee77fb6c38bc9b158d92
Grus <gasper.rus@programmfabrik.de>
2024-02-13 09:46:29 +0100

    Wrap app with UserProvider.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dashboard_bloc_tdd/core/common/app/providers/user_provider.dart';
 import 'package:dashboard_bloc_tdd/core/res/colors.dart';
 import 'package:dashboard_bloc_tdd/core/res/fonts.dart';
 import 'package:dashboard_bloc_tdd/core/services/injection_container.dart';
@@ -5,6 +6,7 @@ import 'package:dashboard_bloc_tdd/core/services/router.dart';
 import 'package:dashboard_bloc_tdd/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,17 +24,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Dashboard TDD',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(accentColor: Colours.primaryColour),
-        fontFamily: Fonts.poppins,
-        appBarTheme: const AppBarTheme(color: Colors.transparent),
-        useMaterial3: true,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return ChangeNotifierProvider(
+      create: (_) => UserProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Dashboard TDD',
+        theme: ThemeData(
+          colorScheme:
+              ColorScheme.fromSwatch(accentColor: Colours.primaryColour),
+          fontFamily: Fonts.poppins,
+          appBarTheme: const AppBarTheme(color: Colors.transparent),
+          useMaterial3: true,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        onGenerateRoute: generateRoute,
       ),
-      onGenerateRoute: generateRoute,
     );
   }
 }
