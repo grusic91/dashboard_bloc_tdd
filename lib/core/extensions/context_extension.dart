94f4ba1fbe16f01c2d47417bccfeaff27028ed7a
Grus <gasper.rus@programmfabrik.de>
2024-02-13 09:46:06 +0100

    Adds UserProvider, currentUser to context extensions.

--- lib/core/extensions/context_extension.dart
+++ lib/core/extensions/context_extension.dart
@@ -1,4 +1,7 @@
+import 'package:dashboard_bloc_tdd/core/common/app/providers/user_provider.dart';
+import 'package:dashboard_bloc_tdd/src/auth/domain/entities/user.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 extension ContextExt on BuildContext {
   // Theme
@@ -15,4 +18,8 @@ extension ContextExt on BuildContext {
 
   // get height
   double get height => size.height;
+
+  UserProvider get userProvider => read<UserProvider>();
+
+  LocalUser? get currentUser => userProvider.user;
 }
