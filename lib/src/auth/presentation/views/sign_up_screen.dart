872ed00dcc4b700c6cc0553af4563db39bba7083
Grus <gasper.rus@programmfabrik.de>
2024-02-14 20:12:46 +0100

    configure SignUpScreen with form and UX

--- lib/src/auth/presentation/views/sign_up_screen.dart
+++ lib/src/auth/presentation/views/sign_up_screen.dart
@@ -1,5 +1,17 @@
-import 'package:dashboard_bloc_tdd/core/extensions/context_extension.dart';
+import 'package:dashboard_bloc_tdd/core/common/app/providers/user_provider.dart';
+import 'package:dashboard_bloc_tdd/core/common/widgets/gradient_background.dart';
+import 'package:dashboard_bloc_tdd/core/common/widgets/rounded_button.dart';
+import 'package:dashboard_bloc_tdd/core/res/fonts.dart';
+import 'package:dashboard_bloc_tdd/core/res/media_res.dart';
+import 'package:dashboard_bloc_tdd/core/utils/core_utils.dart';
+import 'package:dashboard_bloc_tdd/src/auth/data/models/user_model.dart';
+import 'package:dashboard_bloc_tdd/src/auth/presentation/bloc/auth_bloc.dart';
+import 'package:dashboard_bloc_tdd/src/auth/presentation/views/sign_in_screen.dart';
+import 'package:dashboard_bloc_tdd/src/auth/presentation/widgets/sign_up_form.dart';
+import 'package:dashboard_bloc_tdd/src/dashboard/presentation/views/dashboard.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -11,14 +23,119 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final fullNameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    fullNameController.dispose();
+    confirmPasswordController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Placeholder(
-      child: Center(
-        child: Text(
-          'SIGN UP SCREEN',
-          style: context.theme.textTheme.bodyLarge,
-        ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (_, state) {
+          if (state is AuthError) {
+            CoreUitls.showSnackBar(context, state.message);
+          } else if (state is SignedUp) {
+            context.read<AuthBloc>().add(
+                  SignInEvent(
+                    email: emailController.text.trim(),
+                    password: passwordController.text.trim(),
+                  ),
+                );
+          } else if (state is SignedIn) {
+            context.read<UserProvider>().initUser(state.user as LocalUserModel);
+            Navigator.pushReplacementNamed(context, Dashboard.routeName);
+          }
+        },
+        builder: (BuildContext context, AuthState state) {
+          return GradientBackground(
+            image: MediaRes.authGradientBackground,
+            child: SafeArea(
+              child: Center(
+                child: ListView(
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  children: [
+                    const Text(
+                      'Easy to learn, discover more skills.',
+                      style: TextStyle(
+                        fontFamily: Fonts.aeonik,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 32,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      'Sign up for an account',
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.of(context)
+                              .pushReplacementNamed(SignInScreen.routeName);
+                        },
+                        child: const Text('Already have an account?'),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    SignUpForm(
+                      fullNameController: fullNameController,
+                      emailController: emailController,
+                      passwordController: passwordController,
+                      confirmPasswordController: confirmPasswordController,
+                      formKey: formKey,
+                    ),
+                    const SizedBox(height: 30),
+                    if (state is AuthLoading)
+                      const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    else
+                      RoundedButton(
+                        label: 'Sign Up',
+                        onPressed: () {
+                          // hide keyboard if onpened
+                          FocusManager.instance.primaryFocus?.unfocus();
+                          // get fresh user setup with reloading
+                          FirebaseAuth.instance.currentUser?.reload();
+                          if (formKey.currentState!.validate()) {
+                            SignUpEvent(
+                              email: emailController.text.trim(),
+                              password: passwordController.text.trim(),
+                              name: fullNameController.text.trim(),
+                            );
+                          }
+                        },
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
