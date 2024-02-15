cf6f4a9de5f7b11ef69798acaf90eb886e93aa98
Grus <gasper.rus@programmfabrik.de>
2024-02-16 00:09:46 +0100

    Adds NestedBackButton to core.

---
+++ lib/core/common/widgets/nested_back_button.dart
@@ -0,0 +1,32 @@
+import 'package:dashboard_bloc_tdd/core/extensions/context_extension.dart';
+import 'package:flutter/material.dart';
+
+class NestedBackButton extends StatelessWidget {
+  const NestedBackButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: deprecated_member_use
+    return PopScope(
+      onPopInvoked: (_) async {
+        try {
+          context.pop();
+        } catch (_) {
+          Navigator.of(context).pop();
+        }
+      },
+      child: IconButton(
+        onPressed: () {
+          try {
+            context.pop();
+          } catch (_) {
+            Navigator.of(context).pop();
+          }
+        },
+        icon: Theme.of(context).platform == TargetPlatform.iOS
+            ? const Icon(Icons.arrow_back_ios_new)
+            : const Icon(Icons.arrow_back),
+      ),
+    );
+  }
+}
