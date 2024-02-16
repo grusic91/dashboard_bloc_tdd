9858824654fc39d29a86243109608fbef1020895
Grus <gasper.rus@programmfabrik.de>
2024-02-16 12:45:57 +0100

    Adds course usecases test get courses.

---
+++ test/core/common/features/course/domain/usecases/course_repo.mock.dart
@@ -0,0 +1,4 @@
+import 'package:dashboard_bloc_tdd/core/common/features/course/domain/repos/course_repo.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockCourseRepo extends Mock implements CoursRepo {}
