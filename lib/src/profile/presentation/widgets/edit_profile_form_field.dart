465ba35dac452358162e8a9d4a8567d39aa30162
Grus <gasper.rus@programmfabrik.de>
2024-02-16 11:55:58 +0100

    Adds EditProfileFormField.

---
+++ lib/src/profile/presentation/widgets/edit_profile_form_field.dart
@@ -0,0 +1,40 @@
+import 'package:dashboard_bloc_tdd/core/common/widgets/input_field.dart';
+import 'package:flutter/material.dart';
+
+class EditProfileFormField extends StatelessWidget {
+  const EditProfileFormField({
+    required this.fieldTitle,
+    required this.controller,
+    required this.hintText,
+    super.key,
+  });
+
+  final String fieldTitle;
+  final TextEditingController controller;
+  final String hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Text(
+            fieldTitle,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 12,
+            ),
+          ),
+        ),
+        const SizedBox(height: 10),
+        IField(
+          controller: controller,
+          hintText: hintText,
+        ),
+        const SizedBox(height: 30),
+      ],
+    );
+  }
+}
